8a562f4954362c6eb725b2ba8a7d06680dd9b3fa
SahooBishwajeet <biswa.2004.bdk@gmail.com>
2024-11-25 20:13:24 +0530

    [Add] Create Results component to display accuracy and error metrics with animations

---
+++ src/components/Results.tsx
@@ -0,0 +1,66 @@
+import { motion } from 'framer-motion';
+
+const Results = ({
+  errors,
+  accuracyPercent,
+  total,
+  className,
+}: {
+    errors: number,
+    accuracyPercent: number,
+    total: number,
+    className?: string
+  }) => {
+
+  const initial = { opacity: 0 }
+  const animate = { opacity: 1 }
+  const transition = { duration: 1 }
+
+  return (
+    <motion.ul
+      className="flex flex-col items-center text-amber-400 space-y-3"
+    >
+      <motion.li
+        initial={initial}
+        animate={animate}
+        transition={{ ...transition, delay: 0 }}
+        className="text-xl font-semibold">
+
+        Results
+
+      </motion.li>
+
+      <motion.li
+        initial={initial}
+        animate={animate}
+        transition={{ ...transition, delay: 0.5 }}
+      >
+
+        Accuracy : {accuracyPercent}
+
+      </motion.li>
+
+      <motion.li
+        initial={initial}
+        animate={animate}
+        transition={{ ...transition, delay: 1 }}
+        className="text-red-400">
+
+        Errors : {errors}
+
+      </motion.li>
+
+      <motion.li
+        initial={initial}
+        animate={animate}
+        transition={{ ...transition, delay: 1.5 }}
+      >
+
+        Total : {total}
+
+      </motion.li>
+    </motion.ul>
+  );
+}
+
+export default Results;
