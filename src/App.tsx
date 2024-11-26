97721ab3c1ed803f82f68ffbdb2789b8811a2386
SahooBishwajeet <biswa.2004.bdk@gmail.com>
2024-11-26 15:57:07 +0530

    [Add] Integrate Results and Typing components into App, implement restart functionality for word generation

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,43 @@
 import { faker } from '@faker-js/faker';
+import { useState } from 'react';
 import RestartButton from './components/RestartButton';
+import Results from './components/Results';
+import Typing from './components/Typing';
 
 const words = faker.word.words(10);
 
 function App() {
+  const [words, setWords] = useState(faker.word.words(10));
+
+  const handleRestart = () => {
+    const newWords = faker.word.words(10);
+    setWords(newWords);
+  }
+
   return (
     <>
       <CountDownTimer seconds={60} />
-      <GeneratedWords words={words} />
-      <RestartButton onRestart={() => {}} className={"mx-auto mt-10 text-slate-500"}/>
+      <WordsConatiner>
+        <GeneratedWords words={words} />
+        <Typing userInput={words} className={"absolute inset-0"} />
+      </WordsConatiner>
+      <RestartButton onRestart={handleRestart} className={"mx-auto mt-10 text-slate-500"} />
+      <Results errors={5} accuracyPercent={90} total={100} className={"mx-auto mt-10"} />
     </>
   );
 }
 
+const WordsConatiner = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className='relative max-w-xl mt-3 text-3xl leading-relaxed break-all'>
+      {children}
+    </div>
+  )
+}
+
 const GeneratedWords = ({ words }: { words: string[] }) => {
   return (
-    <div className='text-4xl text-center text-slate-500'>
+    <div className='text-slate-500'>
       {words}
     </div>
   )
