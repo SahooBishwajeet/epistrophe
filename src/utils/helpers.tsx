75e86427cd82f8846efc96355ceb0f020e7f74da
SahooBishwajeet <biswa.2004.bdk@gmail.com>
2024-11-26 16:55:22 +0530

    [Add] Implement helper functions for error counting and accuracy calculation

--- src/utils/helpers.tsx
+++ src/utils/helpers.tsx
@@ -1,3 +1,23 @@
 export const numberToPercent = (percentage: number) => {
   return `${percentage.toFixed(0)}%`;
 };
+
+export const countErrors = (actual: string, expected: string) => {
+  const expectedChars = expected.split("");
+
+  return expectedChars.reduce((errors, expectedChar, index) => {
+    const actualChar = actual[index];
+
+    if (actualChar !== expectedChar) {
+      errors++;
+    }
+
+    return errors;
+  }, 0);
+};
+
+export const calculateAccuracy = (total: number, errors: number) => {
+  if (total === 0) return 0;
+
+  return ((total - errors) / total) * 100;
+};
