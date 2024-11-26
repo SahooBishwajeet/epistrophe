3c6d5f76f53aeb06df0c8d5e6e2d3bdb01b28853
SahooBishwajeet <biswa.2004.bdk@gmail.com>
2024-11-26 16:35:54 +0530

    [Add] Implement custom useCountDown hook for countdown functionality

---
+++ src/hooks/useCountDown.tsx
@@ -0,0 +1,30 @@
+import { useCallback, useEffect, useRef, useState } from 'react';
+
+const useCountDown = (seconds: number) => {
+  const [timeLeft, setTimeLeft] = useState(seconds);
+  const intervalRef = useRef<NodeJS.Timer | null>(null);
+
+  const startCountDown = useCallback(() => {
+    intervalRef.current = setInterval(() => {
+      setTimeLeft((timeLeft) => timeLeft - 1);
+    }, 1000);
+  }, [setTimeLeft]);
+
+  const resetCountDown = useCallback(() => {
+    if (intervalRef.current) {
+      clearInterval(intervalRef.current);
+    }
+
+    setTimeLeft(seconds);
+  }, [seconds]);
+
+  useEffect(() => {
+    if (!timeLeft && intervalRef.current) {
+      clearInterval(intervalRef.current);
+    }
+  }, [timeLeft, intervalRef]);
+
+  return { timeLeft, startCountDown, resetCountDown };
+};
+
+export default useCountDown;
